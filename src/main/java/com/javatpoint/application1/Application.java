65d2b943dd63cbc60385a914cbe021f6278430c3
DESKTOP-V3O34Q3\Srinivas <kajalkumari8812@gmail.com>
2023-06-15 12:50:56 +0530

    commit

--- src/main/java/com/javatpoint/application1/Application.java
+++ src/main/java/com/javatpoint/application1/Application.java
@@ -1,18 +1,18 @@
 package com.javatpoint.application1;
 
-import java.util.ArrayList;'
-import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
-class Application{
+class Application {
 
-public int cw(String w){
-String[] sw=StringUtils.split(w,' ');
-return (sw==null)? 0:sw.length;
+	public int cw(String w) {
+		String[] sw = StringUtils.split(w, ' ');
+		return (sw == null) ? 0 : sw.length;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		Application a = new Application();
+		int c = a.cw("i have four words");
+		System.out.print(c);
+	}
 }
-public static void main(String[] args){
-System.out.print("hello");
-Application a=new Application();
-int c=a.cw("i have four words");
-System.out.print(c);
-}}
