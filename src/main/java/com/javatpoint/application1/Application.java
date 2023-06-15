89e6b362b4d925c3ef3104ab64e8352ccdbc34e0
DESKTOP-V3O34Q3\Srinivas <kajalkumari8812@gmail.com>
2023-06-15 10:44:00 +0530

    added dependencies

--- src/main/java/com/javatpoint/application1/Application.java
+++ src/main/java/com/javatpoint/application1/Application.java
@@ -1,5 +1,17 @@
 package com.javatpoint.application1;
+
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+
 class Application{
+
+public int cw(String w){
+String[] sw=StringUtils.split(w,' ');
+return (sw==null)? 0:sw.length;
+}
 public static void main(String[] args){
 System.out.print("hello");
+Application a=new Application();
+int c=a.cw("i have four words");
+System.out.print(c);
 }}
