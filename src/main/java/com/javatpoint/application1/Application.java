d1f4760622381c90588926732c907dc834a7ae01
DESKTOP-V3O34Q3\Srinivas <kajalkumari8812@gmail.com>
2023-06-15 11:25:47 +0530

    commit

--- src/main/java/com/javatpoint/application1/Application.java
+++ src/main/java/com/javatpoint/application1/Application.java
@@ -1,6 +1,7 @@
 package com.javatpoint.application1;
 
-import java.util.ArrayList;
+import java.util.ArrayList;'
+import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 
 class Application{
