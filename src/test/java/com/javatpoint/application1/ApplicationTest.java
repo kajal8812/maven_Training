ce98a828645149a48179b6404fea6991a6363c13
DESKTOP-V3O34Q3\Srinivas <kajalkumari8812@gmail.com>
2023-06-15 12:56:02 +0530

    eclipse

---
+++ src/test/java/com/javatpoint/application1/ApplicationTest.java
@@ -0,0 +1,10 @@
+package com.javatpoint.application1;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ApplicationTest{
+
+}
